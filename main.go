32ef1deac7b94383244169af406f59b1cb509c75
sakurakotubaki <gitonly543@gmail.com>
2025-05-28 12:12:35 +0900

    squash merge ok

--- main.go
+++ main.go
@@ -2,17 +2,12 @@ package main
 
 import "fmt"
 
-func logger() {
-	fmt.Println("logger")
-}
-
-func loop() {
-	// 3 for loop
-	for i := 0; i < 3; i++ {
-		logger()
+func tenLoop() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
 	}
 }
 
 func main() {
-  logger()
+	tenLoop()
 }
